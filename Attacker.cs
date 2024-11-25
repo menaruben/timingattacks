1dee4841d0ea9ff6b1608194f88e813d4eaf6ef5
menaruben <ruben@mena.ch>
2024-11-25 15:49:33 +0100

    comments

--- Attacker.cs
+++ Attacker.cs
@@ -1,27 +1,20 @@
 using System.Diagnostics;
 using System.Text;
 
-public sealed class Attacker
-{
+public sealed class Attacker {
     private static readonly int ITERATIONS_PER_CHAR = 40_000_000;
 
-    public string findSecret(Account account, int secretLength, char[] charset)
-    {
-        /*
-        Since we assume that we know the length of the secret we can start by
-        creating a secret with the same length as the original secret and fill
-        it with dashes.
-        */
+    public string findSecret(Account account, int secretLength, char[] charset) {
+        /* initialize guess with secretLength amount ofdashes since we
+        assume that we already know the length of the secret */
         var secret = new StringBuilder(new string('-', secretLength));
         for (int i = 0; i < secretLength; i++)
         {
-            /*
-            find the next character by measuring the time it takes to login
-            and taking the character with the longest time and the replace
-            the current index of our dash-filled secret with the found character
-            until we replace all dashes with the correct characters.
-            */
+            /* find next character by measuring the time it takes to login
+            and taking the character with the highest time */
             var found = findNextChar(account, secret.ToString(), i, charset);
+
+            // replace character at current index with found character
             secret = secret.Replace('-', found, i, 1);
             Console.WriteLine($"Current secret: {secret}");
         }
